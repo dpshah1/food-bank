69f9375e8c34b6d7ddfc00e84d5f5fe763e66313
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2021-07-24 11:40:39 -0700

    added user table in database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    items_donated = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"Username: {self.username} Password: {self.password}"
+
 
 @app.route("/")
 def hello_world():
